a573c64a88307929f9e6f1c571108bcf4c12ed2b
HaoLiang0312 <1225683718@qq.com>
2025-03-21 17:01:26 +1100

    tw面试

--- src/main/java/com/thoughtworks/codepairing/model/ShoppingCart.java
+++ src/main/java/com/thoughtworks/codepairing/model/ShoppingCart.java
@@ -28,12 +28,39 @@ public class ShoppingCart {
             } else if (product.getProductCode().startsWith("DIS_15")) {
                 discount = (product.getPrice() * 0.15);
                 loyaltyPointsEarned += (product.getPrice() / 15);
-            } else {
+            //第一题 20% discount实现
+            }else if (product.getProductCode().startsWith("DIS_20")) {
+                discount = (product.getPrice() * 0.2);
+                loyaltyPointsEarned += (product.getPrice() / 20);
+            //第二题满100减50实现
+                
+            }else if (product.getProductCode().startsWith("DISCOUNT_50_PER_100") ){
+                continue;
+                
+            }else {
                 loyaltyPointsEarned += (product.getPrice() / 5);
             }
 
             totalPrice += product.getPrice() - discount;
         }
+
+        double any100Total = 0;
+
+        for (Product product : products){
+            
+            if (product.getProductCode().startsWith("DISCOUNT_50_PER_100") ){
+                any100Total += product.getPrice();
+            }
+        }
+        if (any100Total >= 100){
+            loyaltyPointsEarned = (int) (loyaltyPointsEarned + any100Total / 5);
+            any100Total =any100Total- any100Total / 100 * 50;
+        }
+        else if (any100Total < 100){
+            loyaltyPointsEarned = (int) (loyaltyPointsEarned + any100Total / 5);
+        }
+        totalPrice =  totalPrice + any100Total ;
+       
 
         return new Order(totalPrice, loyaltyPointsEarned);
     }
--- src/test/java/com/thoughtworks/codepairing/model/ShoppingCartTest.java
+++ src/test/java/com/thoughtworks/codepairing/model/ShoppingCartTest.java
@@ -72,4 +72,86 @@ public class ShoppingCartTest {
 
         assertEquals(6, order.getLoyaltyPoints());
     }
+
+    //第一题 20%折扣测试
+
+    @Test
+    public void shouldCalculatePriceFor20PercentDiscount() {
+        List<Product> products = List.of(new Product(PRICE, "DIS_20_ABCD", PRODUCT));
+        ShoppingCart cart = new ShoppingCart(customer, products);
+        Order order = cart.checkout();
+
+        assertEquals(80.0, order.getTotalPrice(), 0.0);
+    }
+
+    @Test
+    public void shouldCalculateLoyaltyPointsFor20PercentDiscount() {
+        List<Product> products = List.of(new Product(PRICE, "DIS_20_ABCD", PRODUCT));
+        ShoppingCart cart = new ShoppingCart(customer, products);
+        Order order = cart.checkout();
+
+        assertEquals(5, order.getLoyaltyPoints());
+    }
+
+    //第二题 满100减50
+    @Test
+    public void shouldCalculatePriceForAverage100Discount50() {
+        List<Product> products = List.of(new Product(PRICE, "DISCOUNT_50_PER_100", PRODUCT),
+        new Product(PRICE, "DISCOUNT_50_PER_100", PRODUCT),
+        new Product(PRICE, "DIS_20_ABCD", PRODUCT));
+        ShoppingCart cart = new ShoppingCart(customer, products);
+        Order order = cart.checkout();
+
+        assertEquals(180.0, order.getTotalPrice(), 0.0);
+    }
+
+    @Test
+    public void shouldCalculateLoyaltyPointsForAverage100Discount50() {
+        List<Product> products = List.of(new Product(PRICE, "DISCOUNT_50_PER_100", PRODUCT),
+        new Product(PRICE, "DISCOUNT_50_PER_100", PRODUCT),
+        new Product(PRICE, "DIS_20_ABCD", PRODUCT));
+        ShoppingCart cart = new ShoppingCart(customer, products);
+        Order order = cart.checkout();
+
+        assertEquals(45, order.getLoyaltyPoints());
+    }
+
+    //第三题 失败测试
+    @Test
+    public void shouldCalculatePriceForAnyAverage100Discount50() {
+        List<Product> products = List.of(new Product(80, "DISCOUNT_50_PER_100_fail", PRODUCT));
+        ShoppingCart cart = new ShoppingCart(customer, products);
+        Order order = cart.checkout();
+
+        assertEquals(80.0, order.getTotalPrice(), 0.0);
+    }
+
+    @Test
+    public void shouldCalculateLoyaltyPointsForAnyAverage100Discount50() {
+        List<Product> products = List.of(new Product(80, "DISCOUNT_50_PER_100_fail", PRODUCT));
+        ShoppingCart cart = new ShoppingCart(customer, products);
+        Order order = cart.checkout();
+
+        assertEquals(16, order.getLoyaltyPoints());
+    }
+
+    @Test
+    public void shouldCalculatePriceForAnyAverage100Discount50Achieve() {
+        List<Product> products = List.of(new Product(40, "DISCOUNT_50_PER_100_A", PRODUCT),
+        new Product(60, "DISCOUNT_50_PER_100_ B", PRODUCT));
+        ShoppingCart cart = new ShoppingCart(customer, products);
+        Order order = cart.checkout();
+
+        assertEquals(50.0, order.getTotalPrice(), 0.0);
+    }
+
+    @Test
+    public void shouldCalculateLoyaltyPointsForAnyAverage100Discount50Achieve() {
+        List<Product> products = List.of(new Product(40, "DISCOUNT_50_PER_100_A", PRODUCT),
+        new Product(60, "DISCOUNT_50_PER_100_ B", PRODUCT));
+        ShoppingCart cart = new ShoppingCart(customer, products);
+        Order order = cart.checkout();
+
+        assertEquals(20, order.getLoyaltyPoints());
+    }
 }
